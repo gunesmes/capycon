3bc2c63a800935e1cb36761ab4de3c1c230c8dab
Mesut Güneş <gunesmes@gmail.com>
2020-03-27 17:55:49 +0300

    drivers added

--- lib/capy_con.rb
+++ lib/capy_con.rb
@@ -2,9 +2,23 @@ require "capy_con/version"
 
 module CapyCon
   # Your code goes here...
-  if File.file?("features/support/env.rb")
-  	load "features/support/env.rb"
-  else
-  	load "./loading_script.rb"
+  load "loading_script.rb"
+
+  hello = 
+  """\n\n
+  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  *                                                         *
+  *                capycon - CAPYBARA CONSOLE               *
+  *                                                         *
+  *  Default driver is CHROME `:driver_chrome`              *
+  *  You can set the driver by one of the following options *
+  *                                                         *
+  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  \n"""
+
+  puts hello
+
+  Capybara.drivers.each do |key, value| 
+	  puts "   Capybara.current_driver = :#{key.to_s}"
   end
 end
--- lib/capy_con/version.rb
+++ lib/capy_con/version.rb
@@ -1,3 +1,3 @@
 module CapyCon
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
