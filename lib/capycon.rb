df12dba6a49549be9e4f4d2e9a67a384cef5aed8
Mesut Güneş <gunesmes@gmail.com>
2020-04-08 15:56:36 +0300

    terminal message updated

--- lib/capycon.rb
+++ lib/capycon.rb
@@ -4,29 +4,29 @@ require 'capycon/version'
 
 module Capycon
   hello =
-    ''"\n
-    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    *                                                                           *
-    *                capycon - CAPYBARA CONSOLE                                 *
-    *                                                                           *
-    *  Default driver is :driver_iphone6_vertical_visible                       *
-    *  To set the site name as environment variable:                            *
-    *    echo 'export SITE_NAME=http://www.github.com' >> ~/.bash_profile       *
-    *                                                                           *
-    *  To set the driver  as environment variable:                              *
-    *    echo 'export DRIVER=driver_chrome_visible' >> ~/.bash_profile          *
-    *                   OR                                                      *
-    *  You can set the driver by one of the following options                   *
-    *                                                                           *
-    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-    \n"''
+  ''"
+  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  *                                                                           *
+  *                capycon - CAPYBARA CONSOLE                                 *
+  *                                                                           *
+  *  Default driver is :driver_iphone6_vertical_visible                       *
+  *  To set the site name as environment variable:                            *
+  *    echo 'export SITE_NAME=http://www.github.com' >> ~/.bash_profile       *
+  *                                                                           *
+  *  To set the driver  as environment variable:                              *
+  *    echo 'export DRIVER=driver_chrome_visible' >> ~/.bash_profile          *
+  *                   OR                                                      *
+  *  You can set the driver by one of the following options                   *
+  *                                                                           *
+  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  \n"''
 
   load 'loading_script.rb'
 
   puts hello
   Capybara.drivers.each do |key, value|
     if key.to_s.include?('driver')
-      puts "   Capybara.current_driver = :#{key.to_s}"
+      puts "    Capybara.current_driver = :#{key.to_s}"
     end
   end
 
